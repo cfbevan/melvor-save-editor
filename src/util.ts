59d7129a79fb604183a3fad4e4d68ae67c61db23
Charles Bevan <cfbevan@gmail.com>
2022-07-03 11:30:25 -0400

    Add prettier command to yarn

--- src/util.ts
+++ src/util.ts
@@ -1,23 +1,23 @@
-import { ungzip, gzip } from 'pako';
+import { ungzip, gzip } from "pako";
 
 export type MelvorData = {
-    v: number; // version, 17
-    n: number[]; // numberVars
-    b: number[]; // boolVars
-    s: number[][]; // serializeVars
-    ns: number[][][]; // serializeNestedVars
-    o: string[]; // otherVars
-    cd: number[]; // game?
+  v: number; // version, 17
+  n: number[]; // numberVars
+  b: number[]; // boolVars
+  s: number[][]; // serializeVars
+  ns: number[][][]; // serializeNestedVars
+  o: string[]; // otherVars
+  cd: number[]; // game?
 };
-  
 
 export const decryptSave = (save: string): MelvorData => {
-    const dsave: MelvorData = JSON.parse(ungzip(atob(save), { to: 'string' })) as MelvorData;
-    console.log(dsave);
-    return dsave;
-}
+  const dsave: MelvorData = JSON.parse(
+    ungzip(atob(save), { to: "string" })
+  ) as MelvorData;
+  console.log(dsave);
+  return dsave;
+};
 
 export const encryptSave = (save: MelvorData) => {
-    return btoa(gzip(JSON.stringify(save), { to: 'string' }));
-}
-
+  return btoa(gzip(JSON.stringify(save), { to: "string" }));
+};
